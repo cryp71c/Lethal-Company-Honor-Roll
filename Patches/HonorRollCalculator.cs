fe344a9cd1dca66e50622e63b518f430379cc3d9
alexr <alexr@IMAHEKU>
2024-01-05 22:32:59 -0800

    Add project files.

---
+++ Patches/HonorRollCalculator.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonorRoll.Patches
+{
+    [HarmonyPatch(typeof(RoundManager))]
+    internal class HonorRollCalculator
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void honorRoll() {
+            int totalScrapCollectedInQuota;
+            totalScrapCollectedInQuota = RoundManager.Instance.scrapCollectedInLevel;
+
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonorRoll
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class HonorRoll
+    {
+        private const string modGUID = "cryp71c.honor.roll";
+        private const string modName = "Honor Roll";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static HonorRoll instance;
+
+        internal ManualLogSource mls;
+
+        void Awake() {
+            if (instance == null) { 
+                instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            harmony.PatchAll(typeof(HonorRoll));
+
+
+        }
+    }
+}
